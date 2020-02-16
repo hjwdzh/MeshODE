407bfd3ae85ac378cc079a31e3e76924c22cb85a
hjwdzh <hjwdzh@gmail.com>
2020-02-16 13:02:06 -0800

    add test data

---
+++ tools/arap/data/download.sh
@@ -0,0 +1,4 @@
+wget http://download.cs.stanford.edu/orion/deformation_aware_embedding/scan2cad/example/cad.obj
+wget http://download.cs.stanford.edu/orion/deformation_aware_embedding/scan2cad/example/source.obj
+wget http://download.cs.stanford.edu/orion/deformation_aware_embedding/scan2cad/example/reference.obj
+
