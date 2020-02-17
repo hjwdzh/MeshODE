7dfd5b1503a6d429cc5094ed8c20f8a276b91a9d
hjwdzh <hjwdzh@gmail.com>
2020-02-17 00:18:43 -0800

    add a linear solver for meshcover

--- tools/arap/src/meshcover.cc
+++ tools/arap/src/meshcover.cc
@@ -1,5 +1,7 @@
 #include "meshcover.h"
 
+#include <unordered_map>
+
 #include <igl/point_mesh_squared_distance.h>
 
 MeshCover::MeshCover()
@@ -45,12 +47,66 @@ void MeshCover::Cover(const Mesh& watertight, Mesh& cad) {
 void MeshCover::UpdateCover() {
 	if (!watertight)
 		return;
+
+	// Linear Estimation
+	auto& V = cover.V;
+	auto& F = cover.F;
+	std::unordered_map<long long, FT> trips;
+	int num_entries = V.size();
+	MatrixX B = MatrixX::Zero(num_entries, 3);
+	auto add_entry_A = [&](int x, int y, FT w) {
+		long long key = (long long)x * (long long)num_entries + (long long)y;
+		auto it = trips.find(key);
+		if (it == trips.end())
+			trips[key] = w;
+		else
+			it->second += w;
+	};
+	auto add_entry_B = [&](int m, const Vector3& v) {
+		B.row(m) += v;
+	};
+
 	for (int i = 0; i < findices.size(); ++i) {
 		auto& f = watertight->F[findices[i]];
 		const Vector3& v0 = watertight->V[f[0]];
 		const Vector3& v1 = watertight->V[f[1]];
 		const Vector3& v2 = watertight->V[f[2]];
 		Vector3 v = v0 * weights[i][0] + v1 * weights[i][1] + v2 * weights[i][2];
-		cover.V[i] = v;
+		add_entry_A(i, i, 0.25);
+		add_entry_B(i, v);
 	}
+
+	FT regular = 1;
+	for (int i = 0; i < F.size(); ++i) {
+		for (int j = 0; j < 3; ++j) {
+			int v0 = F[i][j];
+			int v1 = F[i][(j + 1) % 3];
+			add_entry_A(v0, v0, regular);
+			add_entry_A(v0, v1, -regular);
+			add_entry_A(v1, v0, -regular);
+			add_entry_A(v1, v1, regular);
+			add_entry_B(v0, regular * (V[v0] - V[v1]));
+			add_entry_B(v1, regular * (V[v1] - V[v0]));
+		}
+	}
+	Eigen::SparseMatrix<FT> A(num_entries, num_entries);
+	std::vector<T> tripletList;
+	for (auto& m : trips) {
+		tripletList.push_back(T(m.first / num_entries, m.first % num_entries, m.second));
+	}
+	A.setFromTriplets(tripletList.begin(), tripletList.end());
+
+	Eigen::SparseLU<Eigen::SparseMatrix<FT>> solver;
+    solver.analyzePattern(A);
+
+    solver.factorize(A);
+
+    //std::vector<Vector3> NV(V.size());
+    for (int j = 0; j < 3; ++j) {
+        VectorX result = solver.solve(B.col(j));
+
+        for (int i = 0; i < result.rows(); ++i) {
+        	V[i][j] = result[i];
+        }
+    }
 }
