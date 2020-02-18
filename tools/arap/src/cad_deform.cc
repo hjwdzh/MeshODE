112dd7e0fbcf83903661b1e3733e8c248ec7b4be
hjwdzh <hjwdzh@gmail.com>
2020-02-17 22:55:17 -0800

    remove the source model from cad_deform arguments

--- tools/arap/src/cad_deform.cc
+++ tools/arap/src/cad_deform.cc
@@ -14,16 +14,15 @@ int MESH_RESOLUTION = 5000;
 // main function
 int main(int argc, char** argv) {	
 	if (argc < 5) {
-		printf("./deform source.obj reference.obj cad.obj output.obj [GRID_RESOLUTION=64] [MESH_RESOLUTION=5000]\n");
+		printf("./deform cad.obj reference.obj output.obj [GRID_RESOLUTION=64] [MESH_RESOLUTION=5000] [lambda=1]\n");
 		return 0;
 	}
 	//Deform source to fit the reference
 
 
-	Mesh src, ref, cad;
-	src.ReadOBJ(argv[1]);
+	Mesh ref, cad;
+	cad.ReadOBJ(argv[1]);
 	ref.ReadOBJ(argv[2]);
-	cad.ReadOBJ(argv[3]);
 
 	Subdivision sub;
 	sub.Subdivide(cad, 1e-2);
@@ -31,28 +30,24 @@ int main(int argc, char** argv) {
 
 	sub.ComputeGeometryNeighbors(1.5e-2);
 
-	MeshCover cover;
-	cover.Cover(src, sub);
-
 	//sub.ComputeGeometryNeighbors(1e-2);
-	if (argc > 5)
-		sscanf(argv[5], "%d", &GRID_RESOLUTION);
+	if (argc > 4)
+		sscanf(argv[4], "%d", &GRID_RESOLUTION);
 
-	if (argc > 6)
-		sscanf(argv[6], "%d", &MESH_RESOLUTION);
+	if (argc > 5)
+		sscanf(argv[5], "%d", &MESH_RESOLUTION);
 
 	FT lambda = 1;
-	if (argc > 7)
-		sscanf(argv[7], "%lf", &lambda);
+	if (argc > 6)
+		sscanf(argv[6], "%lf", &lambda);
 	printf("lambda %lf\n", lambda);
 	//Get number of vertices and faces
-	std::cout<<"Source:\t\t"<<"Num vertices: "<<src.V.size()<<"\tNum faces: "<<src.F.size()<<std::endl;
+	std::cout<<"Source:\t\t"<<"Num vertices: "<<cad.V.size()<<"\tNum faces: "<<cad.F.size()<<std::endl;
 	std::cout<<"Reference:\t"<<"Num vertices: "<<ref.V.size()<<"\tNum faces: "<<ref.F.size()<<std::endl<<std::endl;
 
 	UniformGrid grid(GRID_RESOLUTION);
 	ref.Normalize();
 	sub.subdivide_mesh.ApplyTransform(ref);
-	src.ApplyTransform(ref);
 	ref.ConstructDistanceField(grid);
 	//sub.subdivide_mesh.WriteOBJ("debug.obj");
 	//ref.WriteOBJ("debug1.obj");
@@ -62,6 +57,6 @@ int main(int argc, char** argv) {
 	std::cout<<"Deformed"<<std::endl;
 
 	//cover.UpdateCover(src, sub);
-	sub.subdivide_mesh.WriteOBJ(argv[4]);
+	sub.subdivide_mesh.WriteOBJ(argv[3]);
 	return 0;
 }
