db5b9c7f9cb5b03141a3999339db44391fce68f5
hjwdzh <hjwdzh@gmail.com>
2020-03-24 11:39:30 -0700

    bijective mapping

--- src/python/cad_neural_deform.py
+++ src/python/cad_neural_deform.py
@@ -28,37 +28,36 @@ graph_loss_src2tar = GraphLossLayer(\
 	graph_V_src, graph_E_src, tar_V, tar_F, rigidity)
 
 param_src2tar = graph_loss_src2tar.param_id
-'''
+
 graph_loss_tar2src = GraphLossLayer(\
 	graph_V_tar, graph_E_tar, src_V, src_F, rigidity)
 
 param_tar2src = graph_loss_tar2src.param_id
-'''
 
 from torchdiffeq import odeint
 
 class ODEFunc(nn.Module):
-	def __init__(self):
-		super(ODEFunc, self).__init__()
-		m = 50
-		nlin = nn.ReLU()
-		self.net = nn.Sequential(
-			nn.Linear(4, m),
-			nlin,
-			nn.Linear(m, m),
-			nlin,
-			nn.Linear(m, 3),
-		)
+    def __init__(self):
+        super(ODEFunc, self).__init__()
+        m = 50
+        nlin = nn.ReLU()
+        self.net = nn.Sequential(
+            nn.Linear(4, m),
+            nlin,
+            nn.Linear(m, m),
+            nlin,
+            nn.Linear(m, 3),
+        )
 
-		for m in self.net.modules():
-			if isinstance(m, nn.Linear):
-				nn.init.normal_(m.weight, mean=0, std=1e-1)
-				nn.init.constant_(m.bias, val=0)
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=1e-1)
+                nn.init.constant_(m.bias, val=0)
 
-	def forward(self, t, y):
-		new_t = t.repeat(y.shape[0],1)
-		yt = torch.cat((y,new_t), 1)
-		return self.net(yt-0.5)
+    def forward(self, t, y):
+        new_t = t.repeat(y.shape[0],1)
+        yt = torch.cat((y,new_t), 1)
+        return self.net(yt-0.5)
 
 timing = torch.from_numpy(np.array([0, 1]).astype('float32'))
 timing_inv = torch.from_numpy(np.array([1, 0]).astype('float32'))
@@ -66,52 +65,26 @@ timing_inv = torch.from_numpy(np.array([1, 0]).astype('float32'))
 func = ODEFunc()
 
 optimizer = optim.Adam(func.parameters(), lr=1e-3)
-#graph_V_src = nn.Parameter(graph_V_src)
-#optimizer = optim.Adam([graph_V_src], lr=1e-3)
 
-niter = 1000000
-
-yGrad = torch.zeros(1,1)
-def extract(xVar):
-	global yGrad
-	yGrad = xVar    
+niter = 500
 
 for it in range(0, niter):
 	optimizer.zero_grad()
 
 	graph_V_src_deform = odeint(func, graph_V_src, timing)[1]
-
 	loss_src2tar = graph_loss_src2tar(graph_V_src_deform, graph_E_src)
-	#loss_src2tar = graph_loss_src2tar(graph_V_src, graph_E_src)
-
-	#graph_V_tar_deform = odeint(func, graph_V_tar_view, timing_inv)[1]
-	#graph_V_tar_deform = graph_V_tar_deform.contiguous()[0]
-	#loss_tar2src = graph_loss_tar2src(graph_V_tar_deform, graph_E_tar)
-
-	#loss = loss_src2tar + loss_tar2src
-	loss = loss_src2tar
-
-	#func.net.register_hook(extract)
-	loss.backward()
-	#print(yGrad)
-
-	optimizer.step()
-	if it % 100 == 0 or True:
-		print('iter=%d, loss=%.6f'%(it, loss.item()))
-
-	'''
-	graph_V_src_deform = ForwardDeform(func, graph_V_src)
-	loss_src2tar = graph_loss_src2tar(graph_V_src_deform, graph_E_src)
-
-	graph_V_tar_deform = BackwardDeform(func, graph_V_tar)
+	graph_V_tar_deform = odeint(func, graph_V_tar, timing_inv)[1]
 	loss_tar2src = graph_loss_tar2src(graph_V_tar_deform, graph_E_tar)
 
 	loss = loss_src2tar + loss_tar2src
-	loss.backward()
-	optimizer.step()
-	#if it % 100 == 0:
-	print('iter=%d, loss=%.6f'%(it, loss.item()))
-	'''
 
+	loss.backward()
+
+	optimizer.step()
+	if it % 100 == 0 or True:
+		print('iter=%d, loss_src2tar=%.6f, loss_tar2src=%.6f'%(it, loss_src2tar.item(), loss_tar2src.item()))
+
+
+graph_V_src = torch.from_numpy(odeint(func, graph_V_src, timing)[1].data.numpy())
 Finalize(src_V, src_F, src_E, src_to_graph, graph_V_src, rigidity, param_src2tar)
 pyDeform.SaveMesh(output_path, src_V, src_F)
